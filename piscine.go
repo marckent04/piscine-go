21706d1e973ee6c4d2a6a96e87b85dd9d4fdbb26
Marc-Henry <36090455+marckent04@users.noreply.github.com>
2019-06-10 20:31:54 +0000

    printComb2

--- piscine.go
+++ piscine.go
@@ -5,32 +5,32 @@ import (
 	"strconv"
 )
 
-//PrintComb ...
-func PrintComb() {
-	n1, n2, n3 := 0, 0, 0
+//PrintComb2 ...
+func PrintComb2() {
+	var n1s, n2s string
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			n1s = strconv.Itoa(i) + strconv.Itoa(j)
+			n1, err1 := strconv.Atoi(n1s)
 
-	for a := 0; a <= 9; a++ {
-		for b := 0; b < 10; b++ {
-			if b == 0 {
-				n2 = 0
-			}
-			for c := 0; c < 10; c++ {
-				if c == 0 {
-					n3 = 0
-				}
-				if n3 > n2 && n2 > n1 {
-					if n1 == 7 && n2 == 8 && n3 == 9 {
-						fmt.Println(strconv.Itoa(n1) + strconv.Itoa(n2) + strconv.Itoa(n3))
-					} else {
-						fmt.Print(strconv.Itoa(n1) + strconv.Itoa(n2) + strconv.Itoa(n3) + ", ")
+			for k := 0; k < 10; k++ {
+				for f := 0; f < 10; f++ {
+					n2s = strconv.Itoa(k) + strconv.Itoa(f)
+					n2, err2 := strconv.Atoi(n2s)
+					if n1 == 99 && n2 == 98 {
+						fmt.Println(n1s + " " + n2s)
+					} else if n1 != n2 {
+						fmt.Print(n1s + " " + n2s + ", ")
+					}
+
+					if f == 10 {
+						fmt.Print(err2)
 					}
 				}
-				n3++
-
 			}
-			n2++
+			if j == 10 {
+				fmt.Print(err1)
+			}
 		}
-
-		n1++
 	}
 }

cb47ebaa4207e0bc1b7e2ca37673839b4d4661bc
Marc-Henry <36090455+marckent04@users.noreply.github.com>
2019-06-10 13:40:07 +0000

    raid1a

---
+++ student.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+//Raid1a ...verifier si un nombre est negatif
+func Raid1a(x, y int) {
+	hc := make([]string, x)
+	vc := make([]string, x)
+
+	//horizontal
+	for i := 0; i < x; i++ {
+		if i == 0 {
+			hc[i] = "o"
+		} else if i == (x - 1) {
+			hc[i] = "0"
+		} else {
+			hc[i] = "-"
+		}
+	}
+
+	//verticale
+	for i := 0; i < len(vc); i++ {
+		if i == 0 {
+			vc[i] = "|"
+		} else if i == (len(vc) - 1) {
+			vc[i] = "|"
+		} else {
+			vc[i] = " "
+		}
+	}
+
+	for i := 0; i < x; i++ {
+		if i == (x - 1) {
+			fmt.Println(hc[i])
+		} else {
+			fmt.Print(hc[i])
+		}
+	}
+
+	for a := 0; a < (y - 2); a++ {
+		for i := 0; i < len(vc); i++ {
+			fmt.Print(vc[i])
+		}
+		fmt.Print("\n")
+	}
+
+	for i := 0; i < x; i++ {
+		if i == (x - 1) {
+			fmt.Println(hc[i])
+		} else {
+			fmt.Print(hc[i])
+		}
+	}
+
+}
+
+func main() {
+	Raid1a(1, 5)
+}
--- test/test.go
+++ test/test.go
@@ -1,9 +1,5 @@
 package main
 
-import piscine ".."
-
 func main() {
-	piscine.IsNegative(1)
-	piscine.IsNegative(0)
-	piscine.IsNegative(-1)
+
 }

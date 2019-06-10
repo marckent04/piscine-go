517b2eea50dbd31614b4f5df0171c88fe68d8804
Marc-Henry <36090455+marckent04@users.noreply.github.com>
2019-06-10 20:50:18 +0000

    printComb2 98 99

--- piscine.go
+++ piscine.go
@@ -8,18 +8,20 @@ import (
 //PrintComb2 ...
 func PrintComb2() {
 	var n1s, n2s string
+	var n1, n2 int
+	var err1, err2 error
 	for i := 0; i < 10; i++ {
 		for j := 0; j < 10; j++ {
 			n1s = strconv.Itoa(i) + strconv.Itoa(j)
-			n1, err1 := strconv.Atoi(n1s)
+			n1, err1 = strconv.Atoi(n1s)
 
+			//creation du 2eme nombre
 			for k := 0; k < 10; k++ {
 				for f := 0; f < 10; f++ {
 					n2s = strconv.Itoa(k) + strconv.Itoa(f)
-					n2, err2 := strconv.Atoi(n2s)
-					if n1 == 99 && n2 == 98 {
-						fmt.Println(n1s + " " + n2s)
-					} else if n1 != n2 {
+					n2, err2 = strconv.Atoi(n2s)
+
+					if n1 != n2 {
 						fmt.Print(n1s + " " + n2s + ", ")
 					}
 
@@ -30,6 +32,11 @@ func PrintComb2() {
 			}
 			if j == 10 {
 				fmt.Print(err1)
+			}
+
+			if n1 == 98 && n2 == 99 {
+				fmt.Println(n1s + " " + n2s)
+				break
 			}
 		}
 	}

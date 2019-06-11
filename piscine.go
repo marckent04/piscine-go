a13d15e3c94035846bf0543f4c8eac83a3679a08
Marc-Henry <36090455+marckent04@users.noreply.github.com>
2019-06-11 07:32:45 +0000

    DivMod

--- piscine.go
+++ piscine.go
@@ -1,7 +1,10 @@
 package piscine
 
-//UltimatePointOne ...
-func UltimatePointOne(n ***int) {
-	p := n
-	***p = 1
+//DivMod ...
+func DivMod(a int, b int, div *int, mod *int) {
+	pdiv := div
+	pmod := mod
+
+	*pdiv = a / b
+	*pmod = a % b
 }
